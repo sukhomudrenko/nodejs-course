d090d90895f42b2ccc49c9c9a4c4be466e3c9544
Volodymyr <sukhomudrenkov17@gmail.com>
2026-09-25 23:11:03 +0300

    refactor: drop commented-out storage config from upload middleware

--- hw-08-mongodb-atlas/middleware/uploadMiddleware.mjs
+++ hw-08-mongodb-atlas/middleware/uploadMiddleware.mjs
@@ -1,22 +1,4 @@
-// import multer from 'multer'
-
-// const storage = multer.diskStorage({
-// 	destination: function (req, file, cb) {
-// 		cb(null, 'uploads')
-// 	},
-// 	filename: function (req, file, cb) {
-// 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-// 		cb(null, uniqueSuffix + '-' + file.originalname)
-// 	},
-// })
-
-// const uploadMiddleWare = multer({ storage: storage })
-// export default uploadMiddleWare
-// uploadMiddleware.mjs
-
-// ! new
 import multer from 'multer'
-import path from 'path'
 
 const storage = multer.diskStorage({
 	destination(req, file, cb) {
